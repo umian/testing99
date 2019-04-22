378d1c308ac8ade3f9fd785395435012700c1427
Usman Mian <usman@usmanmian.net>
2019-04-22 11:38:26 -0600

    this is new files

---
+++ json_io.py
@@ -0,0 +1,13 @@
+import json
+
+employees={"Bob":123,"Steve":324,"John":2303}
+
+print(employees)
+
+with open("employees.json","w") as myfile:
+    json.dump(employees,myfile)
+
+with open("employees.json","r") as myfile01:
+    employee02=json.load(myfile01)
+
+print(employee02)
