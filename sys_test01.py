9e4e4ab683a57acbec9bc2e41b1644b50c4394e9
Usman Mian <usman@usmanmian.net>
2019-04-26 15:31:33 -0600

    add math and time modules

--- sys_test01.py
+++ sys_test01.py
@@ -1,4 +1,22 @@
-import sys
+import sys, os, time
+
+print("clearing screen")
+if sys.platform.startswith("win"):
+    os.system("cls")
+else:
+    os.system("clear")
+print("done")
+
+width, heigth = os.get_terminal_size()
+
 print("running on ",sys.platform)
 print("python version", end="")
 print(sys.version_info)
+# print(sys.modules)
+print(os.get_terminal_size())
+print("lenght: ",width)
+print("height: ",heigth)
+
+for x in range(1,11):
+    print(x, time.strftime("%H:%M:%S"))
+    time.sleep(.5)
